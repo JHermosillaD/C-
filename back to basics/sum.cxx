56afe8e6ab18d336c331d31cb11324eeb20fce1d
jhermosillad <hermosilladiaz@gmail.com>
2021-01-10 14:36:09 -0600

    testo upload

--- back to basics/sum.cxx
+++ back to basics/sum.cxx
@@ -2,12 +2,10 @@
  * sum.cxx
  * 
  * Copyright 2019 Eduardo Hermosilla <eduardohermosilla@iMac-de-Eduardo.local>
- * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
  */
 
 #include <iostream> 
