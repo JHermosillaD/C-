d421df3095cf3c46c15cff27fa2382d56337c77a
Jesus Hermosilla <hermosilladiaz@gmail.com>
2021-09-05 18:28:28 -0500

    no image case

--- OpenCV/ImageChannels.cpp
+++ OpenCV/ImageChannels.cpp
@@ -8,6 +8,12 @@ int main() {
 
    Mat_<Vec3b>image;
    image = imread( "/home/jhermosilla/Proyects/C-Party/OpenCV/Images/image.jpg", 1 );
+    if ( !image.data )
+    {
+        printf("No image data \n");
+        return -1;
+    }
+    
    Vec3b pixel = image(10, 29);
 
    cout << "El Vector pixel en la coordenada (10,29) es: " << pixel << endl;  
