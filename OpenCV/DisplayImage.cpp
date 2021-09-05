fe1955560e7ca2f41d086fc6e510e97376f884b7
Jesus Hermosilla <hermosilladiaz@gmail.com>
2021-09-05 17:43:46 -0500

    Set image path

--- OpenCV/DisplayImage.cpp
+++ OpenCV/DisplayImage.cpp
@@ -3,13 +3,13 @@
 using namespace cv;
 int main(int argc, char** argv )
 {
-    if ( argc != 2 )
-    {
-        printf("usage: DisplayImage.out <Image_Path>\n");
-        return -1;
-    }
+//    if ( argc != 2 )
+//    {
+//        printf("usage: DisplayImage.out <Image_Path>\n");
+//        return -1;
+//    }
     Mat image;
-    image = imread( argv[1], 1 );
+    image = imread( "/home/jhermosilla/Proyects/C-Party/OpenCV/Images/image.jpg", 1 );
     if ( !image.data )
     {
         printf("No image data \n");
