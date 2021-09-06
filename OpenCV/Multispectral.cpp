288b985ac5f113f8b7abd8d016a8b2aaaa728ae3
Jesus Hermosilla <hermosilladiaz@gmail.com>
2021-09-06 10:55:14 -0500

    covariance matrix

--- OpenCV/Multispectral.cpp
+++ OpenCV/Multispectral.cpp
@@ -34,6 +34,7 @@ int main() {
 // Vector space
    int n = image.rows*image.cols;
    float x_space [3][n];
+   float x_space_transpose [n][3];
    for(int i = 0; i < n; i++) 
    {
       x_space[0][i] = redvector[i];
@@ -41,16 +42,35 @@ int main() {
       x_space[2][i] = bluevector[i];
    }   
 
+   for (int i = 0; i < 3; ++i)
+   {
+      for (int j = 0; j < n; ++j) 
+      {
+         x_space_transpose[j][i] = x_space[i][j];
+      }
+   }
+
+
 // Mean Pixel Vector
    float m [2] = {}; 
-   float k = image.rows*image.cols;
-   m [0] = (cv::sum( image )[0])/k;
-   m [1] = (cv::sum( image )[1])/k;
-   m [2] = (cv::sum( image )[2])/k;
+   float size_k = image.rows*image.cols;
+   m [0] = (cv::sum( image )[0])/size_k;
+   m [1] = (cv::sum( image )[1])/size_k;
+   m [2] = (cv::sum( image )[2])/size_k;
 
 // Covariance matrix
-   float covariance [2][2];
-
+   float covariance [3][3];
+   for(int i = 0; i < 3; ++i)
+   {
+      for(int j = 0; j < 3; ++j)
+      {
+         covariance[i][j] = 0;
+         for(int k = 0; k < n; ++k)
+            {
+               covariance[i][j] += ( 1/( size_k - 1 )) * (x_space[i][k] - m[j]) * (x_space_transpose[k][j] - m[j]);
+            }
+      }
+   }
 
    myFile_Handler.close();
    return 0;
