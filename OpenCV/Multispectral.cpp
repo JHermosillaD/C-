b5b0bae391932263ee30cc1c41fa938230a2cf8e
Jesus Hermosilla <hermosilladiaz@gmail.com>
2021-09-06 22:51:50 -0500

    add eigen library

--- OpenCV/Multispectral.cpp
+++ OpenCV/Multispectral.cpp
@@ -1,25 +1,34 @@
+#include <eigen3/Eigen/Eigenvalues>
+#include <opencv2/opencv.hpp>
+#include <Eigen/Core>
+#include <iostream>
 #include <stdio.h>
 #include <fstream>
-#include <iostream>
 #include <vector>
 #include <math.h>
-#include <opencv2/opencv.hpp>
 
+using namespace Eigen;
 using namespace std;
 using namespace cv;
 
 int main() {
    
 // Data  
-   std::ofstream myFile_Handler;
-   myFile_Handler.open("Data.txt");
+   ofstream Spectral_space_file;
+   ofstream Spectral_transformations_file;
+   ofstream New_Spectral_space_file;
+   
+   Spectral_space_file.open("Spectral_space.txt");
+   Spectral_transformations_file.open("Spectral_transformations.txt");
+   New_Spectral_space_file.open("New_Spectral_space.txt");
 
 // Pixel data
    Mat_<Vec3b>image;
    image = imread( "/home/jhermosilla/Proyects/C-Party/OpenCV/Images/Minecraft.jpeg", 1 );
-   vector<int> redvector;   
+
    vector<int> greenvector;
    vector<int> bluevector;
+   vector<int> redvector;
 
    for (int i = 0; i < image.rows; i++)
    {
@@ -33,55 +42,53 @@ int main() {
    }
 
 // Vector space
-   int n = image.rows*image.cols;
-   float x_space [3][n];
-   float x_space_transpose [n][3];
-   for(int i = 0; i < n; i++) 
+   const int n = image.rows*image.cols;
+   MatrixXf x_space(3,n);
+   for (int i = 0; i < n; i++) 
    {
-      x_space[0][i] = redvector[i];
-      x_space[1][i] = greenvector[i];
-      x_space[2][i] = bluevector[i];
+      x_space(0,i) = redvector[i];
+      x_space(1,i) = greenvector[i];
+      x_space(2,i) = bluevector[i];
    }   
+   Spectral_space_file << x_space << endl;
 
-   for (int i = 0; i < 3; ++i)
-   {
-      for (int j = 0; j < n; ++j) 
-      {
-         x_space_transpose[j][i] = x_space[i][j];
-      }
-   }
-
-
-// Mean Pixel Vector
-   float m [2] = {}; 
+// Mean Pixel Vector 
    float size_k = image.rows*image.cols;
-   m [0] = (cv::sum( image )[0])/size_k;
-   m [1] = (cv::sum( image )[1])/size_k;
-   m [2] = (cv::sum( image )[2])/size_k;
+   Vector3f m = Vector3f( cv::sum( image )[0],cv::sum( image )[1], cv::sum( image )[2] ) /size_k;
+   Spectral_transformations_file << "Mean pixel vector \n" << m << endl;
 
 // Covariance matrix
-   float covariance [3][3];
-   for(int i = 0; i < 3; ++i)
-   {
-      for(int j = 0; j < 3; ++j)
-      {
-         covariance[i][j] = 0;
-         for(int k = 0; k < n; ++k)
-            {
-               covariance[i][j] += ( 1/( size_k - 1 )) * (x_space[i][k] - m[j]) * (x_space_transpose[k][j] - m[j]);
-            }
-      }
-   }
+   MatrixXf covariance(3,3);
+   covariance = ( 1/( size_k - 1 )) * ((x_space.colwise() - m) * (x_space.colwise() - m).transpose());
+   Spectral_transformations_file << "Covariance matrix \n" << covariance << endl;
+
 // Correlation matrix
-   float correlation [3][3];
+   MatrixXf correlation(3,3);
    for (int i = 0; i < 3; ++i)
    { 
       for (int j = 0; j < 3; ++j)
       {
-         correlation[i][j] = covariance[i][j] / sqrt (covariance[i][i]*covariance[j][j]);
+         correlation(i,j) = covariance(i,j) / sqrt (covariance(i,i)*covariance(j,j));
       }
    }
+   Spectral_transformations_file << "Correlation matrix \n" << correlation << endl;
 
-   myFile_Handler.close();
+// Eigenvalues and eigenvector
+   SelfAdjointEigenSolver<Matrix3f> eigensolver(covariance);
+   Matrix3f cov_eigenvectors = eigensolver.eigenvectors();
+   Spectral_transformations_file << "Eigenvalues \n" << eigensolver.eigenvalues() << endl;
+   Spectral_transformations_file << "Eigenvectors \n" << eigensolver.eigenvectors() << endl;
+
+// Transformation matrix
+   Matrix3f G = cov_eigenvectors.transpose();
+   Spectral_transformations_file << "Transformation matrix \n" << G << endl;
+
+// New non correlated data space
+   MatrixXf y_space = G*x_space;
+   New_Spectral_space_file << y_space << endl;
+
+   Spectral_space_file.close();
+   Spectral_transformations_file.close();
+   New_Spectral_space_file.close();
    return 0;
 }
